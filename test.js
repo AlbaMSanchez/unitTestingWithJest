96f42d461daf38fe96d719d22c8018b536d134d0
AlbaMSanchez <albasompadel@gmail.com>
2023-02-22 12:06:34 +0000

    hasta test 11(no incluido)

---
+++ test.js
@@ -0,0 +1,18 @@
+const { sum } = require('./app.js');
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+    expect(total).toBe(23);
+});
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+test("One euro should be 1.206 dollars", function () {
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
